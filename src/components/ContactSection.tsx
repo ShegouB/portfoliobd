6ea861a9cb3057d5c1e5dbf1bb325fba0c31e146
ShegouB <djagouboris@gmail.com>
2026-04-20 13:43:40 +0100

    env update

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 import { motion } from 'framer-motion';
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 import { ContactFormData } from '@/types';
 
 const socials = [
